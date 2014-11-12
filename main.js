42e37fd923a64c1763eeb29f696649e51d8681e6
Shannon Wynn <shannonwynn@mac.com>
2014-11-12 14:13:35 -0500

    using jquery validation plugin

--- main.js
+++ main.js
@@ -1,22 +1,56 @@
-function checkForm(form)
+(function($,W,D)
 {
-  if(form.username.length < 7 || form.username.length > 26){
-    alert("Username must be more than 7 or less than 26 characters.");
-    form.username.focus();
-    return false;
-  }
+    var JQUERY4U = {};
 
-re = /[@],[.]/;
-  if(!re.test(form.email.value)) {
-    alert("Email must contain @ and .");
-    form.email.focus();
-    return false;
-  }
+    JQUERY4U.UTIL =
+    {
+        setupFormValidation: function()
+        {
+            //form validation rules
+            $("form").validate({
 
-re = /[0-9],[a-z]/;
-  if(!re.test(form.password.value)) {
-    alert("Password must contain a number and letter.");
-    form.password.focus();
-    return false;
-  }
-}
+                rules: {
+                    username: {
+                    required: true,
+                    minlength: 7,
+                    maxlength: 26
+                    },
+                    email: {
+                        required: true,
+                        email: true
+                    },
+                    password: {
+                        required: true,
+                        minlength: 9,
+
+                    },
+
+                messages: {
+                    username: "Please enter a username between 7 and 26 characters.",
+                    password: {
+                        required: "Please provide a password",
+                        minlength: "Please enter a password with at least 9 characters, using at least one number and one letter."
+                    },
+                    email: "Please enter a valid email address",
+                },
+                submitHandler: function(form) {
+                    form.submit();
+                }
+              }
+            });
+        }
+   };
+
+    //when the dom has loaded setup form validation rules
+    $(D).ready(function($) {
+        JQUERY4U.UTIL.setupFormValidation();
+    });
+
+})(jQuery, window, document);
+
+/*jQuery.validator.addMethod('atLeastOne', function (value) {
+  return /[a-z].*[0-9]|[0-9].*[a-z]/i;
+}, "The password should contain at least one letter and one number.");
+});
+  setupFormValidation: function()
+  {*/
